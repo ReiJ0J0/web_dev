1004a59d1f53237dea9e52535aa4ff6699cf0950
MHombre <markreimanh@gmail.com>
2023-01-08 23:50:35 +0800

    yesd

---
+++ db_allz.php
@@ -0,0 +1,15 @@
+<?php
+// signin.php
+
+// Connect to the database
+$host = 'localhost';
+$user = 'root';
+$pass = '';
+$dbname = 'test';
+$conn = mysqli_connect($host, $user, $pass, $dbname);
+
+// Check connection
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+?>
